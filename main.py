0f12d044b79a8cf3293051b051f23c7e02d484b8
Wenzheng Bi <wengzhengnihao@163.com>
2021-05-07 18:56:24 +0800

    vim

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import numpy as np
+from abc import ABCMeta, abstractmethod
 
-class Student:
+class Student(metaclass=ABCMeta):
 	def __init__(self, name, age):
 		self.name = name
 		self.age = age
+    @abstractmethod
 	def fun(self):
 		pass
+class Me(Student):
+    def __init__(self, name, age, gender):
+        Student.__init__(self, name, age)
+        self.gender = gender
+    def fun(self):
+        print('method done')
 def test():
 	print('hello world')
 
