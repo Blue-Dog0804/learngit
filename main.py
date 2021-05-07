d06b284d225c6776380969b5b7c0a647d9e1ba48
Wenzheng Bi <wengzhengnihao@163.com>
2021-05-07 18:30:59 +0800

    updatemain

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Student:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+	def fun(self):
+		pass
+def test():
+	print('hello world')
+
+if __name__ = '__main__':
+	test()
